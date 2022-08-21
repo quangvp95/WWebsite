c2b6eab75dbf5f5863a7412afd5f24c3ef0d13b9
Hanh <nguyenhanh261098@gmail.com>
2022-08-21 20:30:00 +0700

    Hanh add

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function pauseAudio() {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Octo 1, 2022 00:00:00").getTime();
+var countDownDate = new Date("Sept 11, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
